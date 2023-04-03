25f542129a4ac8cd41dbabd335773e3e5cd6e026
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-04-03 13:41:28 +0100

    create empty component for empty cart

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -1,34 +1,44 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Layout, Hero, Section, CartItem } from '../components'
-import {clearCart} from '../redux/features/cart/cartSlice'
+import { clearCart } from '../redux/features/cart/cartSlice'
 
 const Cart = () => {
-  const dispatch = useDispatch();
-  const {cartItems, total} = useSelector((state) => state.cart);
+  const dispatch = useDispatch()
+  const { cartItems, total } = useSelector((state) => state.cart)
 
   return (
-
     <>
       <Hero title="Cart Items" text="List of all cart items" />
       <Layout>
-        <Section>
-          <CartItem items={cartItems}/>
-          <hr />
-          <div className='flex flex-col items-center'>
-            <div className='flex w-full justify-between px-10 py-5 font-bold'>
-              <p>Total</p>
-              <p>${total}</p>
+        {cartItems.length > 0 ? (
+          <Section>
+            <CartItem items={cartItems} />
+            <hr />
+            <div className="flex flex-col items-center">
+              <div className="flex w-full justify-between px-10 py-5 font-bold">
+                <p>Total</p>
+                <p>${total}</p>
+              </div>
+              <div className="flex gap-4">
+                <button
+                  onClick={() => dispatch(clearCart())}
+                  className="text-red-700 font-semibold text-sm md:text-lg border border-red-700 py-1 px-3 hover:bg-red-800 hover:text-white rounded"
+                >
+                  Clear Cart
+                </button>
+                <button className="text-red-700 font-semibold text-sm md:text-lg border border-red-700 py-1 px-3 hover:bg-red-800 hover:text-white rounded">
+                  Proceed to Checkout
+                </button>
+              </div>
             </div>
-            <div className='flex gap-4'>
-              <button onClick={() => dispatch(clearCart())} className='text-red-700 font-semibold text-sm md:text-lg border border-red-700 py-1 px-3 hover:bg-red-800 hover:text-white rounded'>Clear Cart</button>
-              <button className='text-red-700 font-semibold text-sm md:text-lg border border-red-700 py-1 px-3 hover:bg-red-800 hover:text-white rounded'>Proceed to Checkout</button>
-            </div>
-          </div>
-        </Section>
+          </Section>
+        ) : (
+          <h1>Your cart is empty</h1>
+        )}
       </Layout>
     </>
   )
-} 
+}
 
 export default Cart
